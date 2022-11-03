e08574c56fe17e7e5300d958ebba762e09eb4c2f
France <74524576+france-kradev@users.noreply.github.com>
2022-11-03 14:48:43 +0800

    My first android library

---
+++ safeclicklistener/src/main/java/com/app/android/safeclicklistener/SafeClickListener.kt
@@ -0,0 +1,21 @@
+package com.app.android.safeclicklistener
+
+import android.os.SystemClock
+import android.view.View
+
+/**
+ * a click listener to avoid multiple clicks
+ */
+class SafeClickListener(
+    private var defaultInterval: Int = 1000,
+    private val onSafeCLick: (View) -> Unit
+) : View.OnClickListener {
+    private var lastTimeClicked: Long = 0
+    override fun onClick(v: View) {
+        if (SystemClock.elapsedRealtime() - lastTimeClicked < defaultInterval) {
+            return
+        }
+        lastTimeClicked = SystemClock.elapsedRealtime()
+        onSafeCLick(v)
+    }
+}
